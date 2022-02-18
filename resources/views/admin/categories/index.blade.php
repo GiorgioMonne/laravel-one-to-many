e1a8b30c4117ffafd033d2bf85e1795ee33ce84c
Giorgio <1999giorgio.monne@mail.com>
2022-02-18 09:30:49 -1000

    index categorie

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Lista Categorie</div>
+
+                <div class="card-body">
+
+                  <div class="bottoni mb-3">
+
+                    <a href="{{route("posts.create")}}"><button type="button" class="btn btn-success">Crea Post</button></a>
+
+                  </div>
+
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Azioni</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($categories as $category)
+                          <tr>
+                            <td>{{$categories->id}}</td>
+                            <td>{{$categories->title}}</td>
+                            <td>{{$categories->slug}}</td>
+                            <td>
+                              <a href="{{route("categories.show", $categories->id)}}"><button type="button" class="btn btn-info">Info</button></a>
+                              <a href="{{route("categories.edit", $categories->id)}}"><button type="button" class="btn btn-warning">Modifica</button></a>
+                              
+                              <form action="{{route("categories.destroy", $category->id)}}" method="POST">
+                                  @csrf
+                                  @method("DELETE")
+                                  <button type="submit" class="btn btn-danger mt-3">Elimina</button>
+                              </form>
+
+                            </td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
